417a891038c21a75de13260520fc7a754f7e7600
Nguyen Hoang Duy Sang”git config --global user.email nguyenduysang28032004@gmail.com” <nguyenduysang28032004@gmai.com>
2024-11-25 11:54:39 +0700

    danh muc san pham index

--- index.php
+++ index.php
@@ -18,13 +18,13 @@ $lastestItems = executeResult($sql);
   <!-- Slideshow images -->
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://intphcm.com/data/upload/dung-luong-banner-thoi-trang.jpg" class="d-block w-100" alt="Los Angeles">
+      <img src="./assets/photos/slider_1.jpg" class="d-block w-100" alt="Los Angeles">
     </div>
     <div class="carousel-item">
-      <img src="https://t004.gokisoft.com/uploads/2021/07/2-s-1634-banner-web.jpg" class="d-block w-100" alt="Chicago">
+      <img src="./assets/photos/slider_2.jpg" class="d-block w-100" alt="Chicago">
     </div>
     <div class="carousel-item">
-      <img src="https://t004.gokisoft.com/uploads/2021/07/3-s-1634-banner-web.jpg" class="d-block w-100" alt="New York">
+      <img src="./assets/photos/slider_4.jpg" class="d-block w-100" alt="New York">
     </div>
   </div>
 
@@ -63,6 +63,41 @@ $lastestItems = executeResult($sql);
 
 <!-- Danh Muc San Pham -->
 
+<?php
+$count = 0;
+foreach($menuItems as $item) {
+    $sql = "SELECT Product.*, Category.name as category_name FROM Product 
+            LEFT JOIN Category ON Product.category_id = Category.id 
+            WHERE Product.category_id = ".$item['id']." 
+            ORDER BY Product.updated_at DESC LIMIT 0,4";
+    $items = executeResult($sql);
+    if($items == null || count($items) < 4) continue;
+?>
+<div style="background-color: <?=($count++%2 == 0)?'#f7f9fa':''?>;">
+    <div class="container my-5">
+        <h2 class="text-center mb-4"><?=$item['name']?></h2>
+        <div class="row">
+            <?php foreach($items as $pItem) { ?>
+            <div class="col-md-3 col-sm-6 mb-4">
+                <div class="card product-item h-100">
+                    <a href="detail.php?id=<?=$pItem['id']?>"><img class="card-img-top" src="<?=$pItem['thumbnail']?>" alt="<?=$pItem['title']?>" style="height: 220px; object-fit: cover;"></a>
+                    <div class="card-body">
+                        <h6 class="card-title text-muted"><?=$pItem['category_name']?></h6>
+                        <a href="detail.php?id=<?=$pItem['id']?>" class="text-dark">
+                            <h5 class="card-title font-weight-bold"><?=$pItem['title']?></h5>
+                        </a>
+                        <p class="card-text text-danger font-weight-bold"><?=number_format($pItem['discount'])?> VND</p>
+                    </div>
+                    <div class="card-footer p-0">
+                        
+                    </div>
+                </div>
+            </div>
+            <?php } ?>
+        </div>
+    </div>
+</div>
+<?php } ?>
 
 
 <?php require_once('layouts/footer.php'); ?>
